9efd5f0e21f1bcb4c02cdbd5ce293475a6bdad19
lokiteitor <lokiteitor513@gmail.com>
2019-04-15 14:29:49 -0500

    Creacion de rutas necesarias

--- apiGateway/routes/users.js
+++ apiGateway/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// GET
+router.get('/:id',function (req,res,next) {
+  // obtener los datos de un usuario
+  // obtener los datos de un api REST
+  res.end()
+})
+// POST
+router.post('/',function (req,res,next) {
+  // crear nuevos usuarios
+  res.end()
+})
+
+
+// auth 
+router.get('/:id/auth',function (req,res,next) {
+  // crear los guards de autentificacion
+  // atravez de esta apiGateway todas las solicitudes se autentifican
+  // TODO : crear un middleware de auth
+})
 
 module.exports = router;
